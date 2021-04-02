2b5bede789fcdb10e1cd6dc16dcbe8d01271ee8d
a.shoeibi <a.shoeibi@alibaba.ir>
2021-04-02 13:38:38 +0430

    Add project files.

---
+++ Entities/BankAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace RaceConditionExample.Entities
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; private set; }
+        public void Deposit(decimal amount) => Balance += amount;
+        public void Withdraw(decimal amount) => Balance -= amount;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using RaceConditionExample.Entities;
+
+namespace RaceConditionExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Task> tasks = new();
+            BankAccount myBankAccount = new();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Run(() => 
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        myBankAccount.Deposit(100);
+                        Console.WriteLine($"TaskId: {Task.CurrentId} \t ThreadId: {Thread.CurrentThread.ManagedThreadId} \t Depositing bank account (+100) \t Current Balance:${myBankAccount.Balance}");
+                    }
+                }));
+
+                tasks.Add(Task.Run(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        myBankAccount.Withdraw(100);
+                        Console.WriteLine($"TaskId: {Task.CurrentId} \t ThreadId: {Thread.CurrentThread.ManagedThreadId} \t Withdrawing bank account (-100) \t Current Balance:${myBankAccount.Balance}");
+                    }
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+            Console.WriteLine($"Final Balance is {myBankAccount.Balance}");
+        }
+    }
+}
